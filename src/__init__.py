9ebb001f0cb6ae19b1bfbd1b428a12a22ddbb7b7
Ville Kuosmanen <ville@villekuosmanen.com>
2025-09-07 21:00:08 +0100

    OOD detector

--- src/__init__.py
+++ src/__init__.py
@@ -10,15 +10,18 @@ A package for interpretability analysis of physical AI models, including:
 __version__ = "0.1.0"
 
 # Main exports
-from .sae import SAETrainer, SAETrainingConfig, load_sae_model, TokenSampler, TokenSamplerConfig, MultiModalSAE, create_multimodal_sae, prepare_batch_for_bfloat16
+from .sae import SAETrainer, SAETrainingConfig, load_sae_model, TokenSampler, TokenSamplerConfig, MultiModalSAE, create_multimodal_sae, prepare_batch_for_bfloat16, SAEBuilder, load_sae_model_simple
 from .attention_maps import ACTPolicyWithAttention
 from .utils import make_dataset_without_config, get_repo_hash
+from .ood import OODDetector, create_default_ood_params_path
 
 __all__ = [
     # SAE components
     "SAETrainer",
     "SAETrainingConfig",
     "load_sae_model",
+    "SAEBuilder",
+    "load_sae_model_simple",
     # Feature extraction
     "MultiModalSAE", 
     "create_multimodal_sae",
@@ -30,6 +33,9 @@ __all__ = [
     "is_cache_valid",
     "cleanup_invalid_cache",
     "get_cache_status",
+    # OOD Detection
+    "OODDetector",
+    "create_default_ood_params_path", 
     # Attention mapping
     "ACTPolicyWithAttention",
     # Utilities
--- src/sae/__init__.py
+++ src/sae/__init__.py
@@ -9,5 +9,22 @@ from .activation_collector import (
 from .token_sampler import TokenSamplerConfig, TokenSampler
 from .sae import MultiModalSAE, create_multimodal_sae, prepare_batch_for_bfloat16
 from .trainer import SAETrainer, load_sae_model
+from .builder import SAEBuilder, load_sae_model_simple
 
-__all__ = ["SAETrainer", "SAETrainingConfig", "load_sae_model", "TokenSamplerConfig", "TokenSampler", "MultiModalSAE", "create_multimodal_sae", "prepare_batch_for_bfloat16"]
+__all__ = [
+    "SAETrainer", 
+    "SAETrainingConfig", 
+    "load_sae_model", 
+    "SAEBuilder",
+    "load_sae_model_simple",
+    "TokenSamplerConfig", 
+    "TokenSampler", 
+    "MultiModalSAE", 
+    "create_multimodal_sae", 
+    "prepare_batch_for_bfloat16",
+    "collect_and_cache_activations",
+    "create_cached_dataloader",
+    "is_cache_valid",
+    "cleanup_invalid_cache",
+    "get_cache_status",
+]
